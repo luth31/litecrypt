a961938000eb8e6054755b7877c85c5a46615de9
Paul Munteanu <luth@luthcode.net>
2021-04-28 03:19:24 +0300

    [Source/AES] Remove debug information from ExpandKey

--- src/aes.c
+++ src/aes.c
@@ -37,33 +37,15 @@ void ExpandKey(AES_Key* key, AES_RoundKey* rkey) {
     int i;
     for (i = 0; i < key->size; ++i) {
         rkey->data[i] = key->data[i];
-        printf("%08X ", rkey->data[i]);
     }
-    printf("\ni\ttemp\t\tRot\t\tSub\t\tRcon\t\txor rcon\tw\t\tw final\n");
     uint32_t tmp;
     while (i < 4 * (rkey->rounds + 1)) {
-        printf("%d\t", i);
         tmp = rkey->data[i-1];
-        printf("%08X\t", tmp);
-        if (i % key->size == 0) {
-            //tmp = SubWord(RotWord(tmp)) ^ Rcon[i/key->size];
-            tmp = RotWord(tmp);
-            printf("%08X\t", tmp);
+        if (i % key->size == 0)
+            tmp = SubWord(RotWord(tmp)) ^ Rcon[i/key->size];
+        else if (key->size > 6 && i % key->size == 4)
             tmp = SubWord(tmp);
-            printf("%08X\t", tmp);
-            tmp ^= Rcon[i/key->size];
-            printf("%08X\t", Rcon[i/key->size]);
-            printf("%08X\t", tmp);
-        }
-        else if (key->size > 6 && i % key->size == 4) {
-            tmp = SubWord(tmp);
-            printf("%08X\t", tmp);
-        }
-        else {
-            printf("\t\t\t\t\t\t\t\t");
-        }
         rkey->data[i] = rkey->data[i - key->size] ^ tmp;
-        printf("%08X\t%08X\n", rkey->data[i - key->size], rkey->data[i]);
         ++i;
     }
 }
