3b8ad26ba2683033207bdba76155dc62b9768048
Paul Munteanu <luth@luthcode.net>
2021-06-27 14:26:28 +0300

    [Source/AES] Export symbols of public API functions

--- include/aes.h
+++ include/aes.h
@@ -3,6 +3,12 @@
 #include "stdint.h"
 #include "_aes.h"
 
+#ifdef _MSC_VER
+    #define LITECRYPT_API __declspec(dllexport)
+#elif defined(__clang__) || defined(__GNUC__)
+    #define LITECRYPT_API __attribute__((visibility("default")))
+#endif
+
 typedef struct {
     AES_Key* key;
     uint8_t rounds;
@@ -20,8 +26,12 @@ enum AES_KEY_SIZE {
     AES_KEY_256 = 8
 };
 
-AES_Ctx* AES_Init(enum AES_KEY_SIZE key_size, uint32_t* key);
-void AES_Encrypt(AES_Ctx* ctx, uint8_t input[16], uint8_t output[16]);
-void AES_Decrypt(AES_Ctx* ctx, uint8_t input[16], uint8_t output[16]);
-void AES_Finish(AES_Ctx* ctx);
+LITECRYPT_API AES_Ctx* AES_Init(enum AES_KEY_SIZE key_size, uint32_t* key);
+LITECRYPT_API AES_CTR_Ctx* AES_CTR_Init(enum AES_KEY_SIZE key_size, uint32_t* key, uint64_t nonce);
+LITECRYPT_API void AES_GenCtrBlock(AES_CTR_Ctx* ctx, uint8_t output[16]);
+LITECRYPT_API void AES_Encrypt(AES_Ctx* ctx, uint8_t input[16], uint8_t output[16]);
+LITECRYPT_API void AES_Decrypt(AES_Ctx* ctx, uint8_t input[16], uint8_t output[16]);
+LITECRYPT_API void AES_Finish(AES_Ctx* ctx);
+
+
 #endif
